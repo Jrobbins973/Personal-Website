e280f9d5d874113e4ef077b166a94af8be9c3f89
Joe Robbins <joe.robbins.cpt@gmail.com>
2023-01-20 14:37:41 -0500

    added video and github to first two projects

--- Joe-Robbins-Personal-Website/src/components/ProjectDetails.jsx
+++ Joe-Robbins-Personal-Website/src/components/ProjectDetails.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
 function ProjectDetails(props) {
+
+    const newTab = (url) => {
+        window.open(url, '_blank', 'noreferrer');
+    };
+
+
+
     const {project} = props
     return (
         <div>
             
             <div className='grid-items'>
+            <img src ={project.image}/>
             <h2>{project.title}</h2>
             <h4>{project.description}</h4>
-            <img src ={project.image}/>
+            <p><strong>Technology:</strong> {project.technology}</p>
+            <button onClick={() => newTab('https://www.youtube.com/watch?v=N-IjnXJAWWA')}>Video</button>
+            <button onClick={() => newTab('https://github.com/Jrobbins973/Exercise-api')}>GitHub</button>
             </div>
             
         </div>
